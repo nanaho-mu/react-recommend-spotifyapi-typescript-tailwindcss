ab54e6b6a2e0f9a5578d3d01c5273bca1f7fc3d4
nanaho-mu <nanaho.mu0823@gmail.com>
2022-05-07 18:36:02 +0900

    create paramsType

---
+++ src/types/paramsType.ts
@@ -0,0 +1,11 @@
+export type paramsType = {
+  seed_artists: string;
+  min_valence: number;
+  max_valence: number;
+  min_danceability: number;
+  min_energy: number;
+  max_energy: number;
+  target_mode?: number;
+  min_key?: number;
+  max_key?: number;
+}
